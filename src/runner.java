74487b3a194b0dcf2882646d34c5e93984d95238
WhiteDeath820 <allensam0@gmail.com>
2016-03-15 10:09:03 -0600

    init

---
+++ src/runner.java
@@ -0,0 +1,22 @@
+package ncaa;
+
+import ncaa.team;
+
+public class Runner {
+
+  private final String FILE_PATH = "../ncaa_data_2016.csv";
+  public static void main() {
+    readcsv();
+  }
+
+  public static readcsv() {
+      File csv = new File(FILE_PATH);
+  }
+
+  public static fuckThis() {
+    Team something.list = new Team(//some arguments);
+  }
+
+  public static runGame() {
+  }
+}
